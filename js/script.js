8fcba4c8f46bf3cd0498482d36e2668fc4d15d10
SebastianBuftea <sebibuftea85@gmail.com>
2023-12-01 17:28:51 +0100

    [FINISH] finish exercise

--- js/script.js
+++ js/script.js
@@ -23,19 +23,30 @@ randomNumbers=arrayNumberGenerator()
 document.getElementById("hidden_number").innerText=randomNumbers;
 
 
-/* crel la scomparsa del testo e chiedo al utente quali numeri sono stati mostrati */
+// creo la scomparsa del testo 
 setTimeout(function(){
     document.getElementById("hidden_number").innerText="";
+
+    /* creo un secondo timeout in cui chiedo in input i numeri che sono stati inseriti */
         setTimeout(function(){
             let correct=0;
+            let numsIndov=[]
             for(let i=0; i<5; i++){
                 number=parseInt(prompt(`inserisci il  numero ${i+1}° che hai visualizzato`))
                 if(randomNumbers[i]==number){
                     correct++;
+                    numsIndov.push(randomNumbers[i]);
                 }
             }
-            document.getElementById("hidden_number").innerText=`${randomNumbers} i numeri erano questi, ne hai indovinati ${correct}`
+            if(numsIndov.length==0){
+                document.getElementById("hidden_number").innerText=`I numeri erano questi:  ${randomNumbers}, Non ne hai indovinato neanche uno! Aggiorna la pagina e riprova!`
+            }
+            else{
+                document.getElementById("hidden_number").innerText=`I numeri erano questi: ${randomNumbers}, ne hai indovinati ${correct} e sono i seguenti: ${numsIndov}!`
+            }
+           
          },1000)
+  
 },3000)
 
  
